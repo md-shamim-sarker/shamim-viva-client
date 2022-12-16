39fefc8762d3149324a09d2b6aac5e48d7819204
shamimspro <shamimspro@gmail.com>
2022-12-17 01:27:07 +0600

    develop footer and navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
-import React, {useContext} from 'react';
-import {AuthContext} from '../contexts/UserContext';
+import React from 'react';
+import interview from '../assets/interview.svg';
 
 const Home = () => {
-    const {myName} = useContext(AuthContext);
     return (
         <div>
-            Home page
-            {myName}
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={interview} className="w-full lg:w-1/2 rounded-lg" alt='...' />
+                    <div className='w-full lg:w-1/2'>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
