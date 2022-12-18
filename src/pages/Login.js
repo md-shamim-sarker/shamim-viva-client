275bbbcf0605ed41248ace0e417127f15d62e638
shamimspro <shamimspro@gmail.com>
2022-12-18 19:53:42 +0600

    login and registration page design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc';
+import {BsFacebook} from 'react-icons/bs';
+import auth from '../assets/authentication.svg';
 
 const Login = () => {
     return (
-        <div>
-            Login page
+        <div className="hero">
+            <div className="w-full lg:w-1/2 mx-auto hero-content flex-col lg:flex-row-reverse">
+                <div className="w-full lg:w-1/2 text-center lg:text-left">
+                    <img src={auth} alt="..." />
+                </div>
+                <div className="w-full lg:w-1/2 card flex-shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <h2 className='text-center text-2xl font-bold'>Login Now</h2>
+                        <div className="form-control">
+                            <input type="email" placeholder="Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <input type="password" placeholder="Password" className="input input-bordered" />
+                            <Link to={"/"} className="hover:underline text-blue-600">
+                                <small>Forgot password</small>
+                            </Link>
+                        </div>
+                        <div className="form-control">
+                            <button className="btn btn-primary">Login</button>
+                            <small>Don't have an account. Please <Link to={"/registration"} className="hover:underline text-blue-600">Register</Link>.</small>
+                        </div>
+                        <div className="divider">OR</div>
+                        <div className='flex items-center justify-center gap-x-3'>
+                            <Link to={"/"}>
+                                <FcGoogle className='text-[1.5rem]'></FcGoogle>
+                            </Link>
+                            <Link to={"/"}>
+                                <BsFacebook className='text-[1.4rem] text-blue-700'></BsFacebook>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import auth from '../assets/authentication.svg';
 
 const Registration = () => {
     return (
-        <div>
-            Registration page
+        <div className="hero">
+            <div className="w-full lg:w-1/2 mx-auto hero-content flex-col lg:flex-row-reverse">
+                <div className="w-full lg:w-1/2 text-center lg:text-left">
+                    <img src={auth} alt="..." />
+                </div>
+                <div className="w-full lg:w-1/2 card flex-shrink-0 shadow-2xl">
+                    <div className="card-body">
+                        <h2 className='text-center text-2xl font-bold mb-5'>Registration Now</h2>
+                        <div className="form-control">
+                            <input type="text" placeholder="Full Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <input type="email" placeholder="Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <input type="password" placeholder="New Password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <button className="btn btn-primary">Register</button>
+                            <small>Already have an account. Please <Link to={"/login"} className="hover:underline text-blue-600">Login</Link>.</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
